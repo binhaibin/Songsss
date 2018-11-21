f2794f76be9e922ae21fe85807971351c35d6ca0
unknown <binhaibin>
2018-11-21 21:13:06 +0700

    2

---
+++ YoloSong/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoloSong.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<SongCategory> SongCategories { get; set; }
+    }
+}
---
+++ YoloSong/Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoloSong.Models
+{
+    public class Song
+    {
+        public string thumbnail { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Singer { get; set; }
+        public string Author { get; set; }
+        public List<SongCategory> SongCategories { get; set; }
+    }
+}
